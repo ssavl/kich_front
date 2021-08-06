c232494526875bf840190e485e63de939aa21371
ssavl <get-moscow@yandex.ru>
2021-08-06 13:44:40 +0300

    init

---
+++ src/redux/actions/type.js
@@ -0,0 +1,8 @@
+
+// Authentication
+export const AUTH_CHANGE_FIELD = 'AUTH_CHANGE_FIELD'
+export const AUTH_CLOSE_MODAL = 'AUTH_CLOSE_MODAL'
+export const AUTH_SET_USER = 'AUTH_SET_USER'
+
+// Modal
+export const OPEN_AUTH_MODAL = 'OPEN_AUTH_MODAL'
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux'
+import authModalReducer from './authModal'
+
+
+export default combineReducers({authModalReducer})
